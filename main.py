7615c5ce11b25debc3caaa6ea518f4533ce1cbab
kruk mai <Krukavit@gmail.com>
2026-08-26 02:10:40 +0700

    Add health check endpoint

--- main.py
+++ main.py
@@ -93,3 +93,7 @@ def generate():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
+
+@app.route("/health")
+def health():
+    return "OK", 200
